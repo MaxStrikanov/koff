915d53fa00e294b2b46825ed1aaceeef57411c4b
Max Strikanov <maxvell.88@mail.ru>
2023-10-13 16:03:20 +0300

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 import 'normalize.css'
 import './style.scss'
+import 'swiper/css';
+import Swiper from 'swiper';
 
+
+
+const swiperThumbnails = new Swiper(".product-slider-thumbnails", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+new Swiper(".product-slider_main", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".product-arror_next",
+    prevEl: ".product-arror_prev",
+  },
+  thumbs: {
+    swiper: swiperThumbnails,
+  },
+});
